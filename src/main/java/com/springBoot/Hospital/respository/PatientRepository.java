114fd16a7f0a37ba62336fe4affd64caa0505e2c
KhafiaAyyub <khafiaayyub@gmail.com>
2025-08-26 21:15:18 +0530

    JPQL

--- src/main/java/com/springBoot/Hospital/respository/PatientRepository.java
+++ src/main/java/com/springBoot/Hospital/respository/PatientRepository.java
@@ -5,11 +5,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.springBoot.Hospital.Entity.Patient;
 import com.springBoot.Hospital.Entity.Type.BloodGroupType;
+
+import jakarta.transaction.Transactional;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
@@ -36,6 +39,27 @@ public interface PatientRepository extends JpaRepository<Patient, Long> {
 	@Query("select p from Patient p where p.bloodGroup = ?1") //you need to inject as parameter then use @Param
 	List<Patient> findByBloodGroup(@Param("bloodGroup") BloodGroupType bloodGroup);
 	
+	//?1 - sequel injection attack to avoid we use
 	
+	@Query("select p from Patient p where p.birthDate > :birthDate")
+	List<Patient> findByBornAfterDate(@Param("birthDate") LocalDate birthDate);
+	
+	//Group by Query
+	
+	@Query("select p.bloodGroup, Count(p) from Patient p group by p.bloodGroup")
+	List<Object[]> countEachBloodGroupType();
+	//i want all blood group of all patient so, no need of params
+	
+	//using native query - pure sql query
+	//by default native query is false
+	@Query(value = "select * from patient_tbl" , nativeQuery = true)
+	List<Patient> findAllPatients();
+	
+	//update the name with ID
+	//@Modifying is imp.  - update 
+	@Transactional
+	@Modifying
+	@Query("Update Patient p set p.name = :name where p.id = :id")
+	int updateNameWithId(@Param("name") String name, @Param("id") Long id);
 	
 }
--- src/test/java/com/springBoot/Hospital/PatientTests.java
+++ src/test/java/com/springBoot/Hospital/PatientTests.java
@@ -17,56 +17,81 @@ public class PatientTests {
 
 	@Autowired
 	private PatientRepository patientRepository ;
-	
+
 	@Autowired
 	private PatientService patientService ;
-	
+
 	@Test
 	public void testPatientRepository() {
 		List<Patient> patientList = patientRepository.findAll(); // select * from patient; 
 		System.out.println(patientList);
 	}
-	
-	
-//	@Test
-//	public void testTransactionMethods() {
-//		Patient patient =  patientService.getPatientById(1L);
-//		System.out.println(patient);
-//	}
-//	
+
+
+	//	@Test
+	//	public void testTransactionMethods() {
+	//		Patient patient =  patientService.getPatientById(1L);
+	//		System.out.println(patient);
+	//	}
+	//	
 	@Test
 	public void testTransactionMethods() {
 
-	//OR
-//	Patient patient = patientRepository.findByName("John Doe");
-//    List<Patient> patientList = patientRepository.findByBirthDateOrEmail(LocalDate.of(1990,8,27), "john@example.com");
-		
-//    for (Patient patient : patientList) {
-//    	System.out.println(patient);
-//	}
-    
+		//OR
+		//	Patient patient = patientRepository.findByName("John Doe");
+		//    List<Patient> patientList = patientRepository.findByBirthDateOrEmail(LocalDate.of(1990,8,27), "john@example.com");
+
+		//    for (Patient patient : patientList) {
+		//    	System.out.println(patient);
+		//	}
+
 		//LIKE
-//    List<Patient> patientList = patientRepository.findByNameContaining("er");
-//
-//  for (Patient patient : patientList) {
-//	System.out.println(patient);
-//} 
-		
+		//    List<Patient> patientList = patientRepository.findByNameContaining("er");
+		//
+		//  for (Patient patient : patientList) {
+		//	System.out.println(patient);
+		//} 
+
 		//ORDER BY ID DESC
-//		List<Patient> patientList = patientRepository.findByNameContainingOrderByIdDesc("er");
-//    
-//		 for (Patient patient : patientList) {
-//				System.out.println(patient);
-//			}
-    
-		 // Blood group JPQL
-			List<Patient> patientList = patientRepository.findByBloodGroup(BloodGroupType.A_POSITIVE);
-		    
-			 for (Patient patient : patientList) {
-					System.out.println(patient);
-				
-			 }
+		//		List<Patient> patientList = patientRepository.findByNameContainingOrderByIdDesc("er");
+		//    
+		//		 for (Patient patient : patientList) {
+		//				System.out.println(patient);
+		//			}
+
+		// Blood group JPQL
+		//			List<Patient> patientList = patientRepository.findByBloodGroup(BloodGroupType.A_POSITIVE);
+		//		    
+		//			 for (Patient patient : patientList) {
+		//					System.out.println(patient);	
+		//			 }
+
+		// To find the patient who is born after this date 
+		//		List<Patient> patientList = patientRepository.findByBornAfterDate(LocalDate.of(1999,1,1));
+		//
+		//		for (Patient patient : patientList) {
+		//			System.out.println(patient);	
+		//		}
+
+
+		//group by query
+//		List<Object[]> bloodGroupList = patientRepository.countEachBloodGroupType();
+//
+//		for (Object[] objects : bloodGroupList) {
+//			System.out.println(objects[0] + " "+ objects[1]);
+//		}
 		
+		//native query - find All Patient
+//		List<Patient> patientList = patientRepository.findAllPatients();
+//		
+//		for (Patient patient : patientList) {
+//			System.out.println(patient);
+//		}
+
+		//update query
+		
+		int rowsUpdated = patientRepository.updateNameWithId("RUNNN", 1L);
+		System.out.println(rowsUpdated);
 	}
-	
+
 }
